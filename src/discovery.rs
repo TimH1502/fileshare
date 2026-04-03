e56c4f09b514a0ea2d9cb3416ee12f399bdbc410
Tim Hommerding <t.hommerding@t-online.de>
2026-04-03 18:56:46 +0200

    socket.set_reuse_address works on windows too

--- src/discovery.rs
+++ src/discovery.rs
@@ -115,11 +115,6 @@ impl PeerRegistry {
 
 fn make_multicast_socket() -> Result<Socket> {
     let socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
-    // set_reuse_port is needed on Linux and macOS to allow multiple binds to the same port.
-    #[cfg(any(target_os = "windows"))]
-    socket.set_reuse_port(true)?;
-    // It does not exist on unix.
-    #[cfg(any(target_os = "linux", target_os = "macos"))]
     socket.set_reuse_address(true)?;
     socket.set_nonblocking(true)?;
     socket.bind(&SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, MULTICAST_PORT).into())?;
